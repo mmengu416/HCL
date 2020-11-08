989e61cdeed1aceb149e08148b4c15201cc06480
mmengu416 <71239890+mmengu416@users.noreply.github.com>
2020-11-08 13:49:33 -0500

    Update test.java

--- src/main/java/com/hcl/vamsi/test.java
+++ src/main/java/com/hcl/vamsi/test.java
@@ -1,6 +1,7 @@
 public class Test {
 
-  public static void main(Stringf[] args) {
-    System.out.printLn("Hello");
-  }
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+
 }
